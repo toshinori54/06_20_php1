98056d78cddf8a4a9a1a34980a6e026cf1ecf731
toshinori <toshinori.sugiyama.1933@gmail.com>
2020-12-19 00:59:42 +0900

    fifth commit

--- csv_open.php
+++ csv_open.php
@@ -15,3 +15,22 @@ fclose($filer);
 
 //作成した配列を出力
 var_dump($newAry);
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>アンケートCSV</title>
+</head>
+
+<body>
+    <fieldset>
+        <legend>アンケートCSV </legend>
+        <a href="index.php">入力画面</a>
+</body>
+
+</html>
